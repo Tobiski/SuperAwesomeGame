638f4a2c306121721aac1c8b1bc78eb0d7c68808
Marvin D <marv995@web.de>
2011-11-28 15:20:13 +0100

    Made the ImageManager static and implemented it in the game loop. Removed _imageManager from Game, since it can be used by just including it now set up a seperate file for functions to cut line count and improve readability Wow i can make stuff sound like it's a big matter even though it really isn't :D

---
+++ Miscellaneous.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef MISCELLANEOUS_HPP
+#define MISCELLANEOUS_HPP
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
+namespace Framework{
+
+    //CreateWindow
+    //Arguments:
+    //window                                        - A reference to the window that will be (re)initialised
+    //windowVideoMode                               - Self explantory, sf::VideoMode for the window
+    //windowTitle                                   - Title for the window
+    //windowFullscreen                              - if true, window will run in fullscreen
+    void InitWindow(sf::RenderWindow* window, sf::VideoMode windowVideoMode, std::string windowTitle, bool windowFullscreen);
+
+};
+
+#endif
